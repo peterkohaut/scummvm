21224aa73502589e6cbda32e5a79617401df25a6
Peter Kohaut <peter.kohaut@gmail.com>
2024-11-05 22:37:57 +0100

    TINSEL: Fix big-endian for scene loading in Noir

--- engines/tinsel/scene.cpp
+++ engines/tinsel/scene.cpp
@@ -543,15 +543,15 @@ void SetView(int sceneId, int scale) {
 	int i = 0;
 	CAMERA_STRUC *pCamera = (CAMERA_STRUC *)_vm->_handle->LockMem(g_tempStruc.hCamera);
 	for (i = 0; i < g_tempStruc.numCameras; ++i, ++pCamera) {
-		if (sceneId == FROM_32(pCamera->sceneId)) {
+		if (sceneId == (int)FROM_32(pCamera->sceneId)) {
 			_vm->_spriter->SetCamera(
-				pCamera->rotX,
-				pCamera->rotY,
-				pCamera->rotZ,
-				pCamera->posX * SysVar(SV_SPRITER_SCALE),
-				-pCamera->posY * SysVar(SV_SPRITER_SCALE),
-				-pCamera->posZ * SysVar(SV_SPRITER_SCALE),
-				pCamera->aperture
+				FROM_32(pCamera->rotX),
+				FROM_32(pCamera->rotY),
+				FROM_32(pCamera->rotZ),
+				FROM_32(pCamera->posX * SysVar(SV_SPRITER_SCALE)),
+				FROM_32(-pCamera->posY * SysVar(SV_SPRITER_SCALE)),
+				FROM_32(-pCamera->posZ * SysVar(SV_SPRITER_SCALE)),
+				FROM_32(pCamera->aperture)
 			);
 			SetSysVar(SV_SPRITER_SCENE_ID, sceneId);
 			break;
