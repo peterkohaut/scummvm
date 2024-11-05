0fc85104e7c214e1d80ca30e1555e03575a27c16
Peter Kohaut <peter.kohaut@gmail.com>
2024-11-05 22:33:57 +0100

    TINSEL: Add developer option to skip intro

--- engines/tinsel/pcode.cpp
+++ engines/tinsel/pcode.cpp
@@ -158,9 +158,14 @@ static const byte fragment14[] = {OP_LIBCALL | OPSIZE8, 58,
 };
 static const byte fragment15[] = { OP_JMPFALSE | OPSIZE16, FRAGMENT_WORD(154) };
 
-
-static const byte fragment_noir_skip1[] = { OP_IMM + 1, FRAGMENT_DWORD(0x1e000000), (OP_IMM + 1)| OPSIZE16, FRAGMENT_WORD(0x95), (OP_ZERO +1 ), (OP_LIBCALL + 1) | OPSIZE8, 99, OP_HALT +1 };
-static const byte fragment_noir_skip2[] = { (OP_JUMP + 1) | OPSIZE16, FRAGMENT_WORD(0x50f) };
+static const byte fragment_noir_skip_intro_1[] = {
+		OP_IMM + 1, FRAGMENT_DWORD(0x1e000000),
+		(OP_IMM + 1) | OPSIZE16, FRAGMENT_WORD(0x95),
+		(OP_ZERO + 1),
+		(OP_LIBCALL + 1) | OPSIZE8, 99,
+		OP_HALT +1
+};
+static const byte fragment_noir_skip_intro_2[] = { (OP_JUMP + 1) | OPSIZE16, FRAGMENT_WORD(0x50f) };
 
 #undef FRAGMENT_WORD
 
@@ -234,8 +239,10 @@ const WorkaroundEntry workaroundList[] = {
 	// DW1-GRA: Fixes hang in Temple, when trying to use items on the big hammer
 	{TINSEL_V1, false, false, Common::kPlatformUnknown, 276915849, 0x98, sizeof(fragment15), fragment15},
 
-	{TINSEL_V3, false, false, Common::kPlatformUnknown, 0, 0x6a2, sizeof(fragment_noir_skip1), fragment_noir_skip1},
-	{TINSEL_V3, false, false, Common::kPlatformUnknown, 0x1f3dc654, 0x23c, sizeof(fragment_noir_skip2), fragment_noir_skip2},
+#if 0 // DEBUG: skip intro, remove
+	{TINSEL_V3, false, false, Common::kPlatformUnknown, 0, 0x6a2, sizeof(fragment_noir_skip_intro_1), fragment_noir_skip_intro_1},
+	{TINSEL_V3, false, false, Common::kPlatformUnknown, 0x1f3dc654, 0x23c, sizeof(fragment_noir_skip_intro_2), fragment_noir_skip_intro_2},
+#endif
 
 	{TINSEL_V0, false, false, Common::kPlatformUnknown, 0, 0, 0, NULL}
 };
